b05504001cc067b66ca91ab88423a4cdfa200a13
DenisSkendaj <imdenisskendaj@gmail.com>
2023-10-04 22:43:23 +0100

    Task Completed

---
+++ MERN/React/Exercises/first-jsx/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Learn React</li>
+        <li>Learn React</li>
+        <li>Learn React</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
